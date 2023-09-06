95e6574950d75f1bf3b09201805e1d3c0b770e82
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-09-06 18:11:16 +0530

    Added solution - LeetHub

--- Find Transition Point - GFG/find-transition-point.cpp
+++ Find Transition Point - GFG/find-transition-point.cpp
@@ -23,7 +23,23 @@ int main() {
 
 int transitionPoint(int arr[], int n) {
     // code here
-    int index= upper_bound(arr, arr+n, 0)-arr;
-    if(index==n) return -1;
-    return index;
+    // int index= upper_bound(arr, arr+n, 0)-arr;
+    // if(index==n) return -1;
+    // return index;
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] >= 1) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; 
+        }
+    }
+    // return ans;
+    if(ans==n) return -1;
+    return ans;
 }
